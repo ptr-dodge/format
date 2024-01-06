74b05c044dc788f40e3efd2e0a5106e689565e6d
Peter Dodge <dodge.peter@student.greenriver.edu>
2024-01-05 17:30:17 -0800

    update python script

--- format.py
+++ format.py
@@ -3,30 +3,58 @@ import sys
 
 # Define the comment patterns
 comment_patterns = [
-    r'^\s*#.*',
-    r'^\s*//.*',
-    r'^\s*--.*',
-    r'^\s*/\*.*?\*/',
-    r"^\s*'''.*?'''",
-    r'^\s*;.*',
-    r'^\s*rem\s.*',
-    r'^\s*<!--.*?-->',
-    r'^\s*/\*.*?\*/',
-    r'^\s*\(\*.*?\*\)',
-    r'^\s*//.*',
+    r'^\s*#.*$',           # Python-style comment
+    r'^\s*//.*$',          # Double-slash comment
+    r'^\s*--.*$',          # Double-dash comment
+    r'/\*.*?\*/',          # C-style block comments
+    r"'''.*?'''",          # Triple single quotes for a comment (Start)
+    r".*'''\s*$",          # Triple single quotes for a comment (End)
+    r'^\s*;.*$',           # Semicolon style comment
+    r'^\s*rem\s.*$',       # Windows 'rem' comment
+    r'^\s*<!--.*?-->$',    # HTML comment
+    r'^\s*//\s*Comment.*$',# JavaDoc-style comments
+    r'^\s*\(%*.*%*\)$',    # Pascal-style comments
 ]
 
 # Read text from the specified file (sys.argv[1])
 file_name = sys.argv[1]
-with open(file_name, 'r+') as file:
-    lines = file.readlines()
+with open(file_name, "r+") as file:
+    content = file.read()
     file.seek(0)  # Move cursor to the beginning of the file
 
-    # Remove lines matching comment patterns and rewrite the file
-    for line in lines:
-        if not any(re.match(pattern, line) for pattern in comment_patterns):
-            if line != '\n':
-                print(line)
-                file.write(line)
+    lines = content.split('\n')
+    modified_content = []
+    in_multiline_comment = False
 
-    file.truncate()  # Truncate any remaining content after the new written content
+    for line in lines:
+        skip_line = False
+
+        if re.match(r'/\*.*', line):  # C-style block comment start
+            in_multiline_comment = True
+            skip_line = True
+
+        if re.match(r'.*\*/\s*$', line):  # C-style block comment end
+            in_multiline_comment = False
+            skip_line = True
+
+        if re.match(r"'''.*", line):  # Triple single quotes for a comment (Start)
+            in_multiline_comment = not in_multiline_comment
+            skip_line = True
+
+        if re.match(r".*'''\s*$", line):  # Triple single quotes for a comment (End)
+            in_multiline_comment = not in_multiline_comment
+            skip_line = True
+
+        if in_multiline_comment:
+            skip_line = True
+        else:
+            for pattern in comment_patterns:
+                if re.match(pattern, line):
+                    skip_line = True
+                    break
+
+        if not skip_line:
+            modified_content.append(line)
+
+    file.write('\n'.join(modified_content))
+    file.truncate()
